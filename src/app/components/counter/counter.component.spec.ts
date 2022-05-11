8aab7ce646176f9c6df334bb523cdfe3eae8bd6b
Nhu Ly Huynh <nhu.ly@positivethinking.tech>
2022-05-11 12:34:25 +0700

    add more case test

--- src/app/components/counter/counter.component.spec.ts
+++ src/app/components/counter/counter.component.spec.ts
@@ -1,5 +1,10 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import userEvent from '@testing-library/user-event';
+import { fireEvent, render, screen } from '@testing-library/angular';
 import { CounterComponent } from './counter.component';
 
 const startCount = 123;
@@ -100,5 +105,46 @@ describe('Counter Component', () => {
       By.css('[data-testid="count"]')
     );
     expect(countOutput.nativeElement.textContent).toBe(String(startCount));
+  });
+});
+
+describe('Counter Component with Testing Library', () => {
+  beforeEach(async () => {
+    await render(CounterComponent, {
+      imports: [MatDialogModule, FormsModule],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
+  });
+
+  it('increments the count', () => {
+    const incrementButton = screen.getByText('+');
+
+    fireEvent.click(incrementButton);
+
+    const countOutput = screen.getByTestId('count');
+    expect(countOutput.innerText).toBe('1');
+  });
+
+  it('decrements the count', () => {
+    const decrementButton = screen.getByText('-');
+
+    fireEvent.click(decrementButton);
+
+    const countOutput = screen.getByTestId('count');
+    expect(countOutput.innerText).toBe('-1');
+  });
+
+  it('does not reset if the value is not a number', () => {
+    const value = 'not a number';
+
+    const resetInput = screen.getByTestId('reset-input');
+
+    userEvent.type(resetInput, value);
+
+    const resetButton = screen.getByTestId('reset-button');
+    fireEvent.click(resetButton);
+
+    const countOutput = screen.getByTestId('count');
+    expect(countOutput.innerText).toBe('0');
   });
 });
--- src/app/components/sign-in/sign-in.component.spec.ts
+++ src/app/components/sign-in/sign-in.component.spec.ts
@@ -2,23 +2,26 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { render, screen, fireEvent } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
+import { DialogComponent } from '../dialog/dialog.component';
 import { SignInComponent } from './sign-in.component';
 
-describe('SignIn', () => {
+describe('SignIn Component With Testing Library', () => {
   beforeEach(async () => {
     await render(SignInComponent, {
       imports: [MatDialogModule, FormsModule],
+      declarations: [DialogComponent],
       schemas: [NO_ERRORS_SCHEMA],
     });
   });
 
-  it('should render elements', async () => {
+  it('should render', async () => {
     expect(screen.getAllByText(/username/i)).toBeTruthy();
     expect(screen.getAllByText(/password/i)).toBeTruthy();
     expect(screen.getAllByText(/Sign in/i)).toBeTruthy();
   });
 
-  it('should validation input', async () => {
+  it('should validation inputs', async () => {
     const usernameEle = screen.getByPlaceholderText(/username/i);
     const passwordEle = screen.getByPlaceholderText(/password/i);
 
@@ -30,5 +33,23 @@ describe('SignIn', () => {
 
     expect(usernameEle).toHaveClass('ng-invalid');
     expect(passwordEle).toHaveClass('ng-invalid');
+    expect(screen.getByTestId('submit-button').hasAttribute('disabled')).toBe(
+      true
+    );
+  });
+
+  it('should submit success', async () => {
+    const usernameEle = screen.getByPlaceholderText(/username/i);
+    const passwordEle = screen.getByPlaceholderText(/password/i);
+
+    fireEvent.change(usernameEle, { target: { value: 'angular' } });
+    fireEvent.focusOut(usernameEle);
+
+    fireEvent.change(passwordEle, { target: { value: 'angular@pass' } });
+    fireEvent.focusOut(passwordEle);
+
+    await fireEvent.click(screen.getByTestId('submit-button'));
+    expect(usernameEle.hasAttribute('aria-invalid')).toBeFalse();
+    expect(passwordEle.hasAttribute('aria-invalid')).toBeFalse();
   });
 });
